ad2100753c70dba4c987fa96556d3ac90b55b950
Borja Sahuquillo <e_bsahuq@mercadona.com>
2022-09-06 13:02:24 +0200

    init commit

---
+++ projects/base/webpack.config.js
@@ -0,0 +1,19 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  remotes: {
+    microfront1: "http://localhost:4200/remoteEntry.js",
+    microfront2: "http://localhost:4200/remoteEntry.js",
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
+});
